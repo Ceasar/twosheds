2aa87a86b14bba174a3f2a56f590ec3efa546fba
Ceasar Bautista <cbautista2010@gmail.com>
2013-12-04 15:11:48 -0500

    rewrite tests to use text fixtures

--- test_twosheds.py
+++ test_twosheds.py
@@ -1,123 +1,122 @@
+import pytest
+
 from twosheds.grammar import Grammar
 from twosheds.transform import (AliasTransform,
                                 VariableTransform,
                                 TildeTransform,)
 
 
-def test_alias_substitution1():
-    """Alias substitution should expand aliases."""
-    aliases = {"ls": "ls -G"}
-    transformation = AliasTransform(aliases)
-    text = "ls"
-    assert transformation(text) == "ls -G"
+@pytest.fixture
+def aliases():
+    return {
+        "ls": "ls -G",
+        "home": "cd ~",
+    }
 
 
-def test_alias_substitution2():
-    """Alias substitution should not expand arguments."""
-    aliases = {"ls": "ls -G"}
-    transformation = AliasTransform(aliases)
-    text = "echo ls"
-    assert transformation(text) == "echo ls"
-
-
-def test_alias_substitution_inverse():
-    """Alias substitution should have an inverse."""
-    aliases = {"ls": "ls -G"}
-    transformation = AliasTransform(aliases)
-    text = "ls"
-    assert transformation(transformation(text), inverse=True) == text
-
-
-def test_variable_substitution():
-    environment = {"HOME": "/user/twosheds"}
-    transformation = VariableTransform(environment)
-    text = "cd $HOME"
-    assert transformation(text) == "cd /user/twosheds"
-
-
-def test_variable_substitution_inverse():
-    environment = {"HOME": "/user/twosheds"}
-    transformation = VariableTransform(environment)
-    text = "cd /user/twosheds"
-    assert transformation(text, inverse=True) == "cd $HOME"
-
-
-def test_variable_substitution_order():
-    """Variable substitution should substitute longer values first."""
-    environment = {
+@pytest.fixture
+def environment():
+    return {
         "HOME": "/user/twosheds",
         "LOGNAME": "twosheds",
     }
-    transformation = VariableTransform(environment)
+
+
+@pytest.fixture
+def alias_transform(aliases):
+    return AliasTransform(aliases)
+
+
+@pytest.fixture
+def variable_transform(environment):
+    return VariableTransform(environment)
+
+
+@pytest.fixture
+def tilde_transform():
+    return TildeTransform()
+
+
+@pytest.fixture
+def grammar(alias_transform, tilde_transform, variable_transform):
+    transforms = [
+        alias_transform,
+        tilde_transform,
+        variable_transform,
+    ]
+    return Grammar(transforms=transforms)
+
+
+def test_alias_substitution1(alias_transform):
+    """Alias substitution should expand aliases."""
+    text = "ls"
+    assert alias_transform(text) == "ls -G"
+
+
+def test_alias_substitution2(alias_transform):
+    """Alias substitution should not expand arguments."""
+    text = "echo ls"
+    assert alias_transform(text) == text
+
+
+def test_alias_substitution_inverse(alias_transform):
+    """Alias substitution should have an inverse."""
+    text = "ls"
+    assert alias_transform(alias_transform(text), inverse=True) == text
+
+
+def test_variable_substitution(variable_transform):
+    text = "cd $HOME"
+    assert variable_transform(text) == "cd /user/twosheds"
+
+
+def test_variable_substitution_inverse(variable_transform):
     text = "cd /user/twosheds"
-    assert transformation(text, inverse=True) == "cd $HOME"
+    assert variable_transform(text, inverse=True) == "cd $HOME"
 
 
-def test_variable_substitution_id():
-    environment = {"HOME": "/user/twosheds"}
-    transformation = VariableTransform(environment)
+def test_variable_substitution_order(variable_transform):
+    """Variable substitution should substitute longer values first."""
+    text = "cd /user/twosheds"
+    assert variable_transform(text, inverse=True) == "cd $HOME"
+
+
+def test_variable_substitution_id(variable_transform):
     text = "$HOME"
-    assert transformation(transformation(text), inverse=True) == text
+    assert variable_transform(variable_transform(text), inverse=True) == text
 
 
-def test_tilde_substitution1():
+def test_tilde_substitution1(tilde_transform):
     """Tilde substitution should expand ``~`` as ``$HOME``."""
-    transformation = TildeTransform()
     text = "cd ~/Desktop"
-    assert transformation(text) == "cd $HOME/Desktop"
+    assert tilde_transform(text) == "cd $HOME/Desktop"
 
 
-def test_tilde_substitution2():
+def test_tilde_substitution2(tilde_transform):
     """
     Tilde substitution should not expand a tilde unless it is the prefix
     of a token.
     """
-    transformation = TildeTransform()
     text = "git rebase -i HEAD~3"
-    assert transformation(text) == text
+    assert tilde_transform(text) == text
 
 
-def test_tilde_substitution_inverse():
+def test_tilde_substitution_inverse(tilde_transform):
     """Tilde substitution should have an inverse."""
-    transformation = TildeTransform()
     text = "cd ~/Desktop"
-    assert transformation(transformation(text), inverse=True) == text
+    assert tilde_transform(tilde_transform(text), inverse=True) == text
 
 
-def test_grammar_expand_transform():
-    aliases = {"home": "cd ~"}
-    environment = {"HOME": "/user/twosheds"}
-    transforms = [
-        AliasTransform(aliases),
-        TildeTransform(),
-        VariableTransform(environment),
-    ]
+def test_grammar_transform(grammar):
     text = "home"
-    grammar = Grammar(transforms=transforms)
     assert grammar.transform(text) == "cd /user/twosheds"
 
 
-def test_grammar_expand_inverse():
-    aliases = {"home": "cd ~"}
-    environment = {"HOME": "/user/twosheds"}
-    transforms = [
-        AliasTransform(aliases),
-        TildeTransform(),
-        VariableTransform(environment),
-    ]
+def test_grammar_transform_inverse(grammar):
     text = "cd /user/twosheds"
-    grammar = Grammar(transforms=transforms)
     assert grammar.transform(text, inverse=True) == "home"
 
 
-def test_grammar_expand_id():
-    aliases = {"home": "cd ~"}
-    environment = {"HOME": "/user/twosheds"}
-    transforms = [
-        AliasTransform(aliases),
-        TildeTransform(),
-        VariableTransform(environment),
-    ]
+def test_grammar_transform_id(grammar):
     text = "home"
-    grammar = Grammar(transforms=transforms)
     assert grammar.transform(grammar.transform(text), inverse=True) == text
